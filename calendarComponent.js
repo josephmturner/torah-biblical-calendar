ed9c03cf36cb45a29ff7db4b8408f1a8bf9e1efe
Joseph Turner <joseph@breatheoutbreathe.in>
2022-04-01 13:39:11 -0700

    add monthNames, gregorianMonthNames, newMoonIndex arrays in Calendar

--- calendarComponent.js
+++ calendarComponent.js
@@ -1,4 +1,7 @@
-// adapted from https://www.geeksforgeeks.org/design-a-calendar-using-html-and-css/
+const monthNames = ["Aviv (Nisan)", "Ziv (Iyyar)", "Sivan", "Tammuz", "Av (Ab)", "Elul", "Ethanim (Tishrei)", "Bul (Cheshavan)", "Kislev (Chislev)", "Tevet (Tebeth)", "Shevat", "Adar I"]
+const gregorianMonthNames = ["March/April", "April/May", "May/June", "June/July", "July/August", "August/September", "September/October", "October/November", "November/December", "December/January", "January/February", "February/March"]
+const newMoonIndex = ["(1st New Moon)", "(2nd New Moon)", "(3rd New Moon)", "(4th New Moon)", "(5th New Moon)", "(6th New Moon)", "(7th New Moon)", "(8th New Moon)", "(9th New Moon)", "(10th New Moon)", "(11th New Moon)", "(12th New Moon)"]
+
 const calendarTemplate = `
   <h2 class="month" align="center"></h2>
   <table align="center">
