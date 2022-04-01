d08f7a093fde616bede31bd8f41bcaafb2c068b2
Joseph Turner <joseph@breatheoutbreathe.in>
2022-03-31 21:15:08 -0700

    update rollup config to use plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'index.js',
   output: {
-    dir: 'dist',
+    file: 'bundle.js',
+    format: 'cjs'
   },
-  plugins: [nodeResolve()]
+  plugins: [nodeResolve(), commonjs({
+    include: [ "./index.js", "node_modules/**" ],
+  })],
+  external: ['suncalc', 'timezone-support']
 };
