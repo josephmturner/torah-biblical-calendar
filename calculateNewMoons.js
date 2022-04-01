6d37e28a40509a913eb9d474a16e6e4ac83234ee
Joseph Turner <joseph@breatheoutbreathe.in>
2022-04-01 12:08:26 -0700

    change project copyright name to biblical-lunisolar-calendar

--- calculateNewMoons.js
+++ calculateNewMoons.js
@@ -1,18 +1,18 @@
 /*
   Copyright (C) 2022 by Joseph Turner
 
-  calculate-new-moon-dates is free software: you can redistribute it and/or modify
+  biblical-lunisolar-calendar is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  calculate-new-moon-dates is distributed in the hope that it will be useful,
+  biblical-lunisolar-calendar is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with calculate-new-moon-dates.  If not, see <https://www.gnu.org/licenses/>.
+  along with biblical-lunisolar-calendar.  If not, see <https://www.gnu.org/licenses/>.
 */
 import suncalc from 'suncalc'
 import dateFns from 'date-fns-tz'
