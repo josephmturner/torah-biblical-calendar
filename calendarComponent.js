99cb0e66edc7f173f5e7f6b25a67a6852a2a671c
Joseph Turner <joseph@breatheoutbreathe.in>
2022-04-01 13:25:12 -0700

    add class names to each calendar day

--- calendarComponent.js
+++ calendarComponent.js
@@ -15,41 +15,41 @@ const calendarTemplate = `
     </thead>
     <tbody>
       <tr>
-        <td>8</td>
-        <td>7</td>
-        <td>6</td>
-        <td>5</td>
-        <td>4</td>
-        <td>3</td>
-        <td>2</td>
+        <td class="day8">8</td>
+        <td class="day7">7</td>
+        <td class="day6">6</td>
+        <td class="day5">5</td>
+        <td class="day4">4</td>
+        <td class="day3">3</td>
+        <td class="day2">2</td>
       </tr>
       <tr></tr>
       <tr>
-        <td>15</td>
-        <td>14</td>
-        <td>13</td>
-        <td>12</td>
-        <td>11</td>
-        <td>10</td>
-        <td>9</td>
+        <td class="day15">15</td>
+        <td class="day14">14</td>
+        <td class="day13">13</td>
+        <td class="day12">12</td>
+        <td class="day11">11</td>
+        <td class="day10">10</td>
+        <td class="day9">9</td>
       </tr>
       <tr>
-        <td>22</td>
-        <td>21</td>
-        <td>20</td>
-        <td>19</td>
-        <td>18</td>
-        <td>17</td>
-        <td>16</td>
+        <td class="day22">22</td>
+        <td class="day21">21</td>
+        <td class="day20">20</td>
+        <td class="day19">19</td>
+        <td class="day18">18</td>
+        <td class="day17">17</td>
+        <td class="day16">16</td>
       </tr>
       <tr>
-        <td>29</td>
-        <td>28</td>
-        <td>27</td>
-        <td>26</td>
-        <td>25</td>
-        <td>24</td>
-        <td>23</td>
+        <td class="day29">29</td>
+        <td class="day28">28</td>
+        <td class="day27">27</td>
+        <td class="day26">26</td>
+        <td class="day25">25</td>
+        <td class="day24">24</td>
+        <td class="day23">23</td>
       </tr>
       <tr>
         <td></td>
@@ -58,7 +58,7 @@ const calendarTemplate = `
         <td></td>
         <td></td>
         <td></td>
-        <td>30</td>
+        <td class="day30">30</td>
       </tr>
     </tbody>
   </table>
