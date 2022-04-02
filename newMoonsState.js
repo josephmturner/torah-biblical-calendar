2f58adc7741d29eba376f992a55dce1eb3f71dd0
Joseph Turner <joseph@breatheoutbreathe.in>
2022-04-02 01:07:10 -0700

    add getStartEndDates

--- newMoonsState.js
+++ newMoonsState.js
@@ -1,5 +1,28 @@
 import { calculateNewMoons } from './calculateNewMoons'
+import { addDays } from './dateHelpers'
 
 export const allNewMoons = calculateNewMoons()
 
 export const yearBeginsDate = new Date(allNewMoons[0].getTime())
+
+export function getStartEndDates (newMoonTime) {
+  const firstOfTheYearIndex = allNewMoons.findIndex(el => el.getTime() == newMoonTime)
+
+  const startEndDates = {}
+
+  let i = 1
+  for (const newMoon of allNewMoons.slice(firstOfTheYearIndex)) {
+    if (startEndDates[i-1] !== undefined) {
+      startEndDates[i-1].end = addDays(newMoon, -1)
+    }
+
+    // Include a 13th month in order to display the possible Adar II
+    if (i >= 14) break
+
+    if (startEndDates[i] === undefined) startEndDates[i] = {}
+    startEndDates[i].start = newMoon
+
+    i++
+  }
+  return startEndDates
+}
