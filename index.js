3e7d18a5b94db95ae248286a203ff136927722af
Joseph Turner <joseph@breatheoutbreathe.in>
2022-04-02 02:43:05 -0700

    don't import calculateNewMoons in index.js

--- index.js
+++ index.js
@@ -14,5 +14,4 @@
   You should have received a copy of the GNU General Public License
   along with biblical-lunisolar-calendar.  If not, see <https://www.gnu.org/licenses/>.
 */
-export { calculateNewMoons } from './calculateNewMoons'
 export { Calendar } from './calendarComponent'
